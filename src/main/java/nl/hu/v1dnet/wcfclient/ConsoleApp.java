1a09f732134216977d993d9df055e97c860812e8
Bart van Eijkelenburg <bart.vaneijkelenburg@hu.nl>
2019-05-21 19:01:14 +0200

    Updated ConsoleApp for new WSDL-URL

--- src/main/java/nl/hu/v1dnet/wcfclient/ConsoleApp.java
+++ src/main/java/nl/hu/v1dnet/wcfclient/ConsoleApp.java
@@ -1,14 +1,14 @@
 package nl.hu.v1dnet.wcfclient;
 
-import nl.hu.v1dnet.wcfservice.IMyService;
-import nl.hu.v1dnet.wcfservice.MyService;
+import nl.hu.v1dnet.wcfservice.DateService;
+import nl.hu.v1dnet.wcfservice.IDateService;
 
-public class ConsoleApp 
+public class ConsoleApp
 {
-    public static void main( String[] args )
-    {
-    	IMyService service = new MyService().getBasicHttpBindingIMyService();
-    	
-    	System.out.println(service.getCurrentDateTime());
+    public static void main( String[] args ) {
+        DateService service = new DateService();
+        IDateService proxy = service.getBasicHttpBindingIDateService();
+
+        System.out.println(proxy.getCurrentDateTime());
     }
 }
